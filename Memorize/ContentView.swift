da5becb096d34d6175e2c39891dac7a39cb4960e
Alberto Lagos Toro <alberto.lagos@gmail.com>
2020-05-27 21:19:14 -0400

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Alberto Lagos Toro on 27-05-20.
+//  Copyright © 2020 Alberto Lagos Toro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
