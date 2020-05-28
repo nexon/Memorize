f36872d05eb6873abbeb8ffcc978502b2e51de05
Alberto Lagos Toro <alberto.lagos@gmail.com>
2020-05-27 21:52:25 -0400

    Base Project

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Alberto Lagos Toro on 27-05-20.
+//  Copyright © 2020 Alberto Lagos Toro. All rights reserved.
+//
+
+import Foundation
+
+struct EmojiMemoryGame {
+    var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻","🎃","🕷","😀"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { emojis[$0] }
+    }
+
+    // MARK: - Access to the Model
+    
+    var cards: [MemoryGame<String>.Card] {
+        model.cards
+    }
+
+    // MARK: - Intent(s)
+
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Alberto Lagos Toro on 27-05-20.
+//  Copyright © 2020 Alberto Lagos Toro. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: [Card]
+
+    func choose(card: Card) {
+        print("Chosen card: \(card)")
+    }
+
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = [Card]()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(id: pairIndex*2,  content: content))
+            cards.append(Card(id: pairIndex*2+1, content: content))
+        }
+    }
+
+    struct Card: Identifiable {
+        var id: Int
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+    }
+}
